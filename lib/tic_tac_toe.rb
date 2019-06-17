a8940a2fa22f9196843bb4dc6f81f77878ba0615
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-17 11:15:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,7 +3,7 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
-  WIN_COMBINATIONS = [[0,1,2], [3,4,5],[6,7,8]]
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
@@ -24,5 +24,5 @@ class TicTacToe
   def input_to_index(index)
     input = index + 1
   end
-  
+
 end
