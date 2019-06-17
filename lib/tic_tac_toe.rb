f2762e48c69c8764a01cd659ca4ba121bd9c6a26
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-17 11:25:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,12 +35,66 @@ class TicTacToe
 
   def valid_move?(index)
      index.between?(0,8) && !position_taken?(index)
-   end
+  end
 
+  def turn
+      puts "Please enter 1-9:"
+      input = gets.strip
+      index = input_to_index(input)
+      char = current_player
+      if valid_move?(index)
+        move(index, char)
+        display_board
+      else
+        turn
+      end
+  end
 
+  def won?
+    WIN_COMBINATIONS.detect do |win_combo|
+      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+        return win_combo
+      elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+        return win_combo
+      end
+      false
+    end
+  end
 
+  def full?
+    @board.all?{|occupied| occupied != " "}
+  end
 
+  def draw?
+    !(won?) && (full?)
+  end
 
+  def over?
+    won? || full? || draw?
+  end
 
+  def winner
+    WIN_COMBINATIONS.detect do |win_combo|
+      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
+        return "X"
+      elsif (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
+        return "O"
+      else
+        nil
+      end
+    end
+  end
+  end
+
+  def play
+  while over? == false
+    turn
+  end
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
 
 end
